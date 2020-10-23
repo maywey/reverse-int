70064d3e56db73d45f9c808188d8a32860b6d35a
Taras <sorochynskyi_taras@ukr.net>
2020-10-23 21:55:40 +0300

    feat:done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let numArr = n.toString().split('').filter(i => Number(i) >= 0)
+  let res = []
+  for (let i of numArr) res.unshift(i)
+    return res.join('')
 }
